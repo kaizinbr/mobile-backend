fc3eec88e53942168ac3a1b9cdc6ce9c9f2beebf
kaizin <kaiolucas1812@gmail.com>
2026-02-15 22:10:48 -0300

    ....

--- lib/utils/formatRate.ts
+++
@@ -1,4 +0,0 @@
-export default function formatRate(total: number) {
-    console.log("Formatting rate:", total);
-    return `${Number.isInteger(total) ? total : total.toFixed(1)}/100`;
-}
--- lib/utils/getShorten.ts
+++
@@ -1,9 +0,0 @@
-
-import { customAlphabet } from "nanoid";
-
-const getShorten = (): string => {
-    const nanoid = customAlphabet("6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz", 10);
-    return nanoid();
-};
-
-export default getShorten;
