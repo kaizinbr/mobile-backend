889545f2da5534bc20a773979afd2e0817f83540
kaizin <kaiolucas1812@gmail.com>
2026-03-09 16:35:47 -0300

    rota de users

---
+++ app/api/users/route.ts
@@ -0,0 +1,97 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET(request: Request) {
+    try {
+        const { searchParams } = new URL(request.url);
+        const page = searchParams.get("p");
+        const pageNumber = page ? parseInt(page, 10) : 1;
+        const pageSize = 5;
+        const skip = (pageNumber - 1) * pageSize;
+
+        const usernames = await prisma.profile.findMany({
+            orderBy: {
+                created_at: "desc",
+            },
+            select: {
+                username: true,
+                lowername: true,
+            },
+        });
+
+        const total = await prisma.profile.count({
+            where: {
+                public: true,
+            },
+        });
+
+        return NextResponse.json(
+            {usernames},
+            { status: 200 },
+        );
+    } catch (err) {
+        console.error("fetch error", err);
+        return NextResponse.json(
+            { error: "Failed to fetch reviews" },
+            { status: 500 },
+        );
+    }
+}
+
+// export async function POST(request: Request) {
+//     try {
+//         const { shorten, content, title, tags, raw, published, color, image } =
+//             await request.json();
+
+//         const session = await auth();
+
+//         if (!session?.user?.id) {
+//             return NextResponse.json(
+//                 { error: "Parece que não está autenticado" },
+//                 { status: 401 }
+//             );
+//         }
+
+//         const post = await prisma.post.upsert({
+//             where: { shorten },
+//             update: {
+//                 content,
+//                 title,
+//                 tags: {
+//                     connectOrCreate: tags.map((name: string) => ({
+//                         where: { name },
+//                         create: { name },
+//                     })),
+//                 },
+//                 raw,
+//                 published,
+//                 color,
+//                 image,
+//             },
+//             create: {
+//                 shorten,
+//                 content,
+//                 title,
+//                 tags: {
+//                     connectOrCreate: tags.map((name: string) => ({
+//                         where: { name },
+//                         create: { name },
+//                     })),
+//                 },
+//                 raw,
+//                 published,
+//                 color,
+//                 image,
+//                 authorId: session.user.id,
+//             },
+//         });
+
+//         console.log("post created");
+
+//         return NextResponse.json({ post }, { status: 201 });
+//     } catch (err) {
+//         console.error("upload error", err);
+
+//         return NextResponse.json({ error: "Upload failed" }, { status: 500 });
+//     }
+// }
